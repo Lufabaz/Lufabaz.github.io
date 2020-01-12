6e949272b8beeeb13bb13691e4868b8eb72a0002
Lufabaz <lufabaz@gmail.com>
2020-01-11 22:10:14 -0300

    Exercicio 2 Let Code em JS

--- Curso JS/Livre/LetCode/Exec02/script.js
+++ Curso JS/Livre/LetCode/Exec02/script.js
@@ -1,17 +1,31 @@
 // HTML + Javascript
-
+// Função para cálculo das conversões
 function convert() {
-    let metros = window.document.getElementById('medidaMt')
-    let result = window.document.getElementById('resultCm')
 
-    let convertToCm = Number(numero.value)
+    // Variável de entrada do valor e variável de exibição de resultados
+    let medHtml = window.document.getElementById('medidaOrig')
+    let resultHtml = window.document.getElementById('resultConvert')
 
-    if (compareInt==true) {
-        result.innerHTML+=(`O número digitado é ${numero.value} e o dobro de seu valor é ${numero.value*2}.<br>`)
+    // Variáveis da medida de origem (metros ou centimentros)
+    let idMedOrig = window.document.getElementsByName('radorig')
+    let idMedDest = window.document.getElementsByName('raddest')
 
-    } else {
-        result.innerHTML+=(`<br>O valor ${numero.value} digitado não é um número inteiro.<br> Por favor insira um novo valor.<br><br>`)
-    }
+    let origMts = idMedOrig[0].checked
+    let origCms = idMedOrig[1].checked
+
+    let destMts = idMedDest[0].checked
+    let destCms = idMedDest[1].checked
+
+    if (origMts == true && destCms == true) {
+        let convertToCm = medHtml.value*100
+        resultHtml.innerHTML += `O valor ${medHtml.value} em Metros corresponde a ${convertToCm} em Centímetros <br>`
+    } else if (origCms == true && destMts == true) {
+        let convertToMt = medHtml.value/100
+        resultHtml.innerHTML += `O valor ${medHtml.value} em Centímetros corresponde a ${convertToMt} em Metros <br>`
+} else {
+    resultHtml.innerHTML += `<br>A medida de origem e destino são iguais. Por favor realizar nova conversão.<br><br>`
+}
+
 }
 
 // Limpa o formulário executando um reload na página web após clique do usuário
